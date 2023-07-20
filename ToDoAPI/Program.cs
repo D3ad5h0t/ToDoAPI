a7c3812bffe1ab3f46818fa310ebccce3e66fe67
D3ad5h0t <alexgl007@mail.ru>
2023-07-21 09:45:17 +1000

    Added base api methods

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Data;
+using ToDoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,5 +9,21 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ToDo
 var app = builder.Build();
 
 //app.UseHttpsRedirection();
+
+app.MapGet("api/todo", async (AppDbContext context) =>
+{
+    var items = await context.ToDos.ToListAsync();
+
+    return Results.Ok(items);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDos.AddAsync(toDo);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
+});
 
 app.Run();
